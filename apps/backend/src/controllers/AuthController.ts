3bd3e17b08f2adbf1e0b7ccb2be57160ab9059fa
PPHamster <shinnapatjr@gmail.com>
2023-01-04 22:43:47 +0700

    feat: logout system

--- apps/backend/src/controllers/AuthController.ts
+++ apps/backend/src/controllers/AuthController.ts
@@ -1,50 +1,52 @@
-import { AuthService } from '@/services/AuthService';
 import { GoogleAuthGuard } from '@/utils/guards/GoogleAuthGuard';
 import {
   Controller,
   Get,
   HttpException,
   HttpStatus,
-  Inject,
   Req,
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  public constructor(
-    @Inject('AUTH_SERVICE') private readonly authService: AuthService,
-  ) {}
-
   @Get('google/login')
   @UseGuards(GoogleAuthGuard)
-  public googleLogin(@Res() response: Response) {
-    return response.send({ msg: 'Login Route' });
+  public googleLogin(@Res() res: Response) {
+    return res.send({ msg: 'Login Route' });
   }
 
   @Get('google/callback')
   @UseGuards(GoogleAuthGuard)
-  public googleCallback(@Res() response: Response) {
-    return response.status(HttpStatus.OK).send({ msg: 'Callback Route' });
+  public googleCallback(@Res() res: Response) {
+    return res.status(HttpStatus.OK).send({ msg: 'Callback Route' });
+  }
+
+  @Get('logout')
+  public logout(@Req() req: Request, @Res() res: Response, next: NextFunction) {
+    req.logOut((err: unknown) => {
+      if (err && err instanceof Error) return next(err);
+      res.status(HttpStatus.OK).send({ msg: 'Logout Success' });
+    });
   }
 
   @Get('status')
-  public checkStatus(@Req() request: Request) {
+  public checkStatus(@Req() req: Request) {
     console.log('cookies');
-    console.log(request.session.cookie);
-    if (request.user) {
-      return { msg: 'Authenticate', user: request.user };
+    console.log(req.session.cookie);
+    if (req.user) {
+      return { msg: 'Authenticate', user: req.user };
     } else {
       return { msg: 'Not Authenticate' };
     }
   }
 
   @Get('me')
-  public me(@Req() request: Request, @Res() response: Response) {
-    if (request.user) {
-      response.status(HttpStatus.FOUND).json({ user: request.user });
+  public me(@Req() req: Request, @Res() res: Response) {
+    if (req.user) {
+      res.status(HttpStatus.FOUND).json(req.user);
     } else {
       throw new HttpException('Unauthorization User', HttpStatus.UNAUTHORIZED);
     }
